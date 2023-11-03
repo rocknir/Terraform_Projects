62d7c2facff0da3af4286c9428f9ed2009ca5751
guolei313@gmail.com <guolei313@gmail.com>
2023-11-03 11:51:29 -0400

    adding comments

--- First_EC2Instance/keys.tf
+++ First_EC2Instance/keys.tf
@@ -1,9 +1,13 @@
 variable "AccessKey" {
   type = string
+
+  # put the access key from AWS console here
   default = ""
 }
 
 variable "SecretKey" {
   type = string
+
+  # put the secret key from AWS console here
   default = ""
 }
