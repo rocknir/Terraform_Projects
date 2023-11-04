d5c2247996db2524a276bdc2a4284df4c6faa4ba
guolei313@gmail.com <guolei313@gmail.com>
2023-11-03 21:02:02 -0400

    test file exclusion

--- First_EC2Instance/keys.tf
+++ First_EC2Instance/keys.tf
@@ -2,12 +2,12 @@ variable "AccessKey" {
   type = string
 
   # put the access key from AWS console here
-  default = " "
+  default = ""
 }
 
 variable "SecretKey" {
   type = string
 
   # put the secret key from AWS console here
-  default = " "
+  default = ""
 }
