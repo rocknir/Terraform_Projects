e7ffcdca95ffb213a024eee08dfab9632bc97288
guolei313@gmail.com <guolei313@gmail.com>
2023-11-03 20:26:52 -0400

    remove key content

--- First_EC2Instance/keys.tf
+++ First_EC2Instance/keys.tf
@@ -2,12 +2,12 @@ variable "AccessKey" {
   type = string
 
   # put the access key from AWS console here
-  default = "AKIAVCSHUPOIMO4UTPHN"
+  default = ""
 }
 
 variable "SecretKey" {
   type = string
 
   # put the secret key from AWS console here
-  default = "OJeQWUHjcdwBrNRjsg0TmoV4zOC+hE28LaMgaqIQ"
+  default = ""
 }
